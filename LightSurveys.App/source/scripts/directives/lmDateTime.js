3cbcde9428f52e9feec82090684e3f3927e5de7c
Armin Zia <arminzia@live.com>
2017-07-17 14:09:16 +0430

    add lmDateTime directive

---
+++ LightSurveys.App/source/scripts/directives/lmDateTime.js
@@ -0,0 +1,23 @@
+﻿angular.module('lm.surveys').directive('lmDateTime', [function () {
+    return {
+        restrict: "E",
+        replace: true,
+        template: '<span>{{resultString}}</span>',
+        scope: {
+            value: '='
+        },
+        link: function (scope, element, attrs) {
+            var dateValue = new Date(scope.value);
+            var hours = dateValue.getHours();
+            var minutes = dateValue.getMinutes();
+            var result = '';
+
+            if (hours > 0 || minutes > 0)
+                result = moment(scope.value).format('DD/MM/YYYY hh:mm A');
+            else
+                result = moment(scope.value).format('DD/MM/YYYY');
+
+            scope.resultString = result;
+        }
+    }
+}]);
