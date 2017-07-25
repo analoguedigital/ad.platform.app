dae8cf2aeea8c1cdcc1767e287a4e2c6da3f9dc3
Armin Zia <arminzia@live.com>
2017-07-25 21:17:54 +0430

    implemented the timeline chart

--- LightSurveys.App/source/scripts/controllers/timelineController.js
+++ LightSurveys.App/source/scripts/controllers/timelineController.js
@@ -1,114 +1,168 @@
 'use strict';
 angular.module('lm.surveys').controller('timelineController', ['$scope', 'surveyService', 'alertService', 'gettext', 'userService',
-function ($scope, surveyService, alertService, gettext, userService) {
+    function ($scope, surveyService, alertService, gettext, userService) {
+        $scope.templates = [];
+        $scope.surveys = [];
 
-    $scope.chartOptions = {
-        legend: {
-            display: false,
-        },
-        tooltips: {
-            callbacks: {
-                title: function (items, data) {
-                    return "Records: " + data.datasets[0].meta[items[0].index].records;
-                },
-                label: function (item, data) {
-                    return "Sum Impact: " + data.datasets[0].data[item.index];
-                }
+        $scope.currentDate = new Date();
+        $scope.months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+        $scope.chartLabels = [];
+        $scope.chartDatasets = [];
+
+        $scope.generateChartData = function () {
+            var daysInMonth = moment($scope.currentDate).daysInMonth();
+            var currentDay = moment($scope.currentDate).date();
+            var firstDayOfMonth = moment($scope.currentDate).add(-(currentDay - 1), 'day').toDate();
+            var lastDayOfMonth = moment($scope.currentDate).add((daysInMonth - currentDay), 'day').toDate();
+
+            var days = [];
+            days.push(firstDayOfMonth);
+            for (var i = 2; i <= daysInMonth; i++) {
+                var daysToAdd = -(currentDay - i);
+                var tick = moment($scope.currentDate).add(daysToAdd, 'day').toDate();
+                days.push(tick);
             }
-        }
-    };
 
-    $scope.templates = [];
-    $scope.values = [];
-    $scope.months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-           "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+            $scope.chartLabels = days;
 
-    var readSurveys = function () {
-        return surveyService.getAllSubmittedSurveys()
-        .then(function (surveys) {
-            angular.forEach(surveys, function (survey) {
-                var date = new Date(survey.surveyDate);
+            var datasets = [];
+            _.forEach($scope.templates, (template) => {
+                var data = [];
+                var records = _.filter($scope.surveys, (survey) => { return survey.formTemplateId == template.id });
 
-                var dateFormValue = _.filter(survey.formValues, { 'metricId': survey.formTemplate.calendarDateMetricId })[0];
-                if (dateFormValue) {
-                    date = new Date(dateFormValue.dateValue);
-                }
-                date.setHours(0, 0, 0, 0);
+                console.log('records', records);
 
-                var value = _.filter(survey.formValues, { 'metricId': survey.formTemplate.timelineBarMetricId })[0];
+                _.forEach(days, (day) => {
+                    var foundSurveys = _.filter(records, (record) => {
+                        if (moment(day).format('MM-DD-YYYY') === moment(record.surveyDate).format('MM-DD-YYYY')) {
+                            return record;
+                        }
+                    });
 
-                if (value) {
-                    var existingValueForDateAndTemplate = _.find($scope.values, { 'templateId': survey.formTemplateId, 'date': date });
+                    if (foundSurveys.length) {
+                        console.log('found on ' + moment(day).format('MM-DD'), foundSurveys.length);
 
-                    if (existingValueForDateAndTemplate !== undefined) {
-                        existingValueForDateAndTemplate.value.numericValue = parseInt(existingValueForDateAndTemplate.value.numericValue) + parseInt(value.numericValue);
-                        existingValueForDateAndTemplate.records++;
-                    }
-                    else {
-                        $scope.values.push({
-                            "templateId": survey.formTemplateId,
-                            "date": date,
-                            "value": value,
-                            "colour": survey.formTemplate.colour,
-                            "records": 1,
+                        let impactSum = 0;
+                        _.forEach(foundSurveys, (survey) => {
+                            var timelineBarFormValue = _.filter(survey.formValues, { 'metricId': survey.formTemplate.timelineBarMetricId })[0];
+                            if (timelineBarFormValue) {
+                                impactSum += parseInt(timelineBarFormValue.numericValue);
+                            }
                         });
+
+                        data.push(impactSum);
+                    } else {
+                        data.push(0);
                     }
-                }
+                });
+
+                var ds = {
+                    label: template.title,
+                    backgroundColor: template.colour,
+                    borderColor: template.colour,
+                    borderWidth: 2,
+                    data: data,
+                    stack: 1
+                };
+
+                datasets.push(ds);
             });
-        })
-    }
 
-    readSurveys()
-     .then(function () {
-         $scope.currentDate = new Date(new Date().setDate(1));
-         $scope.$watch('currentDate', $scope.generateChart);
-     });
-
-    $scope.generateChart = function () {
-
-        var data = {};
-        data.labels = [];
-
-        var daysInMonth = new Date($scope.currentDate.getYear(), $scope.currentDate.getMonth() + 1, 0).getDate();
-
-        var currentMonthValues = _.filter($scope.values, function (val) {
-            return val.date.getYear() === $scope.currentDate.getYear() && val.date.getMonth() === $scope.currentDate.getMonth()
-        });
-
-        currentMonthValues = _.sortBy(currentMonthValues, function (val) { return val.date.toISOString(); });
-
-        data.datasets = [{
-            label: "Recordings",
-            backgroundColor: [],
-            borderColor: [],
-            data: [],
-            meta: [],
-        }];
-
-        angular.forEach(currentMonthValues, function (val) {
-            data.labels.push(val.date.getDate());
-            data.datasets[0].data.push(val.value.numericValue);
-            data.datasets[0].meta.push({ records: val.records });
-            data.datasets[0].backgroundColor.push(val.colour);
-            data.datasets[0].borderColor.push(val.colour);
-        });
-
-        while (data.labels.length < 30) {
-            data.labels.push("");
-            data.datasets[0].data.push(0);
+            $scope.chartDatasets = datasets;
         }
 
+        $scope.generateChart = function () {
+            $scope.generateChartData();
 
-        $scope.timelineData = data;
-    };
+            var ctx = document.getElementById("timeline").getContext("2d");
+            var chartOptions = {
+                tooltips: {
+                    mode: 'index',
+                    callbacks: {
+                        title: function (items, data) {
+                            var xLabel = items[0].xLabel;
+                            var yValue = 0;
+                            _.forEach(items, (item) => {
+                                yValue += parseInt(item.yLabel);
+                            });
 
-    $scope.nextMonth = function () {
-        $scope.currentDate = moment($scope.currentDate).add(1, 'months').toDate();
-    }
+                            var result = [];
+                            result.push(xLabel);
+                            result.push(`Impact: ${yValue}`);
 
-    $scope.previousMonth = function () {
-        $scope.currentDate = moment($scope.currentDate).subtract(1, 'months').toDate();
-    }
+                            return result;
+                        },
+                        label: function (item, data) {
+                            var label = data.datasets[item.datasetIndex].label;
+                            return `${label}: ${item.yLabel}`;
+                        }
+                    }
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        time: {
+                            unit: 'day',
+                            displayFormats: {
+                                day: 'lll'
+                            }
+                        },
+                        ticks: {
+                            autoSkip: true,
+                            callback: function (value) {
+                                return moment(value).format('MMM D');
+                            },
+                        }
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            max: 30
+                        }
+                    }]
+                }
+            };
 
+            var config = {
+                type: 'groupableBar',
+                data: {
+                    labels: $scope.chartLabels,
+                    datasets: $scope.chartDatasets
+                },
+                options: chartOptions
+            };
 
-}]);
+            if ($scope.timelineChart)
+                $scope.timelineChart.destroy();
+
+            $scope.timelineChart = new Chart(ctx, config);
+        };
+
+        $scope.nextMonth = function () {
+            $scope.currentDate = moment($scope.currentDate).add(1, 'months').toDate();
+        }
+
+        $scope.previousMonth = function () {
+            $scope.currentDate = moment($scope.currentDate).subtract(1, 'months').toDate();
+        }
+
+        $scope.readSurveys = function () {
+            surveyService.getAllSubmittedSurveys()
+                .then(function (surveys) {
+                    // get unique form templates
+                    var templates = _.uniqBy(surveys, 'formTemplate').map((survey) => { return survey.formTemplate; });
+
+                    $scope.templates = templates;
+                    $scope.surveys = surveys;
+                });
+        }
+
+        $scope.activate = function () {
+            $scope.readSurveys();
+            $scope.$watch('currentDate', $scope.generateChart);
+        }
+
+        $scope.activate();
+
+    }]);
