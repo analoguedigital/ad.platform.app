7cdefbe52e08a27ee7985819ee656d08aae28ea5
Armin Zia <arminzia@live.com>
2017-07-29 14:03:26 +0430

    refactored timeline into a directive

---
+++ LightSurveys.App/source/scripts/directives/lmTimeline.js
@@ -0,0 +1,171 @@
+﻿angular.module('lm.surveys').directive('lmTimeline', ['$rootScope', function ($rootScope) {
+    return {
+        restrict: "E",
+        replace: true,
+        template: '<canvas id="timeline"></canvas>',
+        scope: {
+            formTemplates: '=',
+            surveys: '=',
+            height: '@'
+        },
+        link: function (scope, element, attrs) {
+            scope.currentDate = new Date();
+
+            if (scope.height && scope.height.length) {
+                element.css('height', scope.height + 'px');
+            }
+
+            function generateTimelineData() {
+                var daysInMonth = moment(scope.currentDate).daysInMonth();
+                var currentDay = moment(scope.currentDate).date();
+                var firstDayOfMonth = moment(scope.currentDate).add(-(currentDay - 1), 'day').toDate();
+                var lastDayOfMonth = moment(scope.currentDate).add((daysInMonth - currentDay), 'day').toDate();
+
+                var days = [];
+                days.push(firstDayOfMonth);
+                for (var i = 2; i <= daysInMonth; i++) {
+                    var daysToAdd = -(currentDay - i);
+                    var tick = moment(scope.currentDate).add(daysToAdd, 'day').toDate();
+                    days.push(tick);
+                }
+
+                scope.chartLabels = days;
+
+                var datasets = [];
+                _.forEach(scope.formTemplates, (template) => {
+                    var data = [];
+                    var records = _.filter(scope.surveys, (survey) => { return survey.formTemplateId == template.id });
+
+                    _.forEach(days, (day) => {
+                        var foundSurveys = _.filter(records, (record) => {
+                            if (moment(day).format('MM-DD-YYYY') === moment(record.surveyDate).format('MM-DD-YYYY')) {
+                                return record;
+                            }
+                        });
+
+                        if (foundSurveys.length) {
+                            let impactSum = 0;
+                            _.forEach(foundSurveys, (survey) => {
+                                var timelineBarFormValue = _.filter(survey.formValues, { 'metricId': template.timelineBarMetricId })[0];
+                                if (timelineBarFormValue) {
+                                    impactSum += parseInt(timelineBarFormValue.numericValue);
+                                }
+                            });
+
+                            data.push(impactSum);
+                        } else {
+                            data.push(0);
+                        }
+                    });
+
+                    var ds = {
+                        label: template.title,
+                        backgroundColor: template.colour,
+                        borderColor: template.colour,
+                        borderWidth: 2,
+                        data: data,
+                        stack: 1
+                    };
+
+                    datasets.push(ds);
+                });
+
+                scope.chartDatasets = datasets;
+            }
+
+            function renderTimelineChart() {
+                var canvas = element[0];
+                var ctx = canvas.getContext('2d');
+
+                var chartOptions = {
+                    responsive: true,
+                    maintainAspectRatio: true,
+                    tooltips: {
+                        mode: 'index',
+                        callbacks: {
+                            title: function (items, data) {
+                                var xLabel = items[0].xLabel;
+                                var yValue = 0;
+                                _.forEach(items, (item) => {
+                                    yValue += parseInt(item.yLabel);
+                                });
+
+                                var result = [];
+                                result.push(xLabel);
+                                result.push(`Impact: ${yValue}`);
+
+                                return result;
+                            },
+                            label: function (item, data) {
+                                var label = data.datasets[item.datasetIndex].label;
+                                return `${label}: ${item.yLabel}`;
+                            }
+                        }
+                    },
+                    scales: {
+                        xAxes: [{
+                            display: true,
+                            time: {
+                                unit: 'day',
+                                displayFormats: {
+                                    day: 'lll'
+                                }
+                            },
+                            ticks: {
+                                autoSkip: true,
+                                callback: function (value) {
+                                    return moment(value).format('MMM D');
+                                },
+                            }
+                        }],
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true,
+                                max: 100
+                            }
+                        }]
+                    }
+                };
+
+                var config = {
+                    type: 'groupableBar',
+                    data: {
+                        labels: scope.chartLabels,
+                        datasets: scope.chartDatasets
+                    },
+                    options: chartOptions
+                };
+
+                if (scope.timelineChart)
+                    scope.timelineChart.destroy();
+
+                scope.timelineChart = new Chart(ctx, config);
+            }
+
+            scope.timelineNextMonth = function () {
+                scope.currentDate = moment(scope.currentDate).add(1, 'months').toDate();
+            }
+
+            scope.timelinePreviousMonth = function () {
+                scope.currentDate = moment(scope.currentDate).subtract(1, 'months').toDate();
+            }
+
+            scope.$watchGroup(['formTemplates', 'surveys'], () => {
+                generateTimelineData();
+                renderTimelineChart();
+            });
+
+            scope.$watch('currentDate', () => {
+                generateTimelineData();
+                renderTimelineChart();
+            });
+
+            $rootScope.$on('timeline-next-month', () => {
+                scope.timelineNextMonth();
+            });
+            $rootScope.$on('timeline-previous-month', () => {
+                scope.timelinePreviousMonth();
+            });
+        }
+    }
+}]);
