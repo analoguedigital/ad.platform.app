f8e578398902881862efa0a772866f2f5ca2717c
Armin Zia <arminzia@live.com>
2017-08-06 19:25:56 +0430

    implemented custom tooltips. overlay issue is resolved.

--- LightSurveys.App/source/scripts/directives/lmTimeline.js
+++ LightSurveys.App/source/scripts/directives/lmTimeline.js
@@ -245,6 +245,91 @@
             scope.chartDatasets = generateDatasets(ticks);
         }
 
+        function customTooltips(tooltip) {
+            // Tooltip Element
+            var tooltipEl = document.getElementById('chartjs-tooltip');
+
+            if (!tooltipEl) {
+                tooltipEl = document.createElement('div');
+                tooltipEl.id = 'chartjs-tooltip';
+                tooltipEl.innerHTML = "<table></table>"
+                this._chart.canvas.parentNode.appendChild(tooltipEl);
+            }
+
+            // Hide if no tooltip
+            if (tooltip.opacity === 0) {
+                tooltipEl.style.opacity = 0;
+                return;
+            }
+
+            // Set caret Position
+            tooltipEl.classList.remove('above', 'below', 'no-transform');
+            if (tooltip.yAlign) {
+                tooltipEl.classList.add(tooltip.yAlign);
+            } else {
+                tooltipEl.classList.add('no-transform');
+            }
+
+            function getBody(bodyItem) {
+                return bodyItem.lines;
+            }
+
+            // Set Text
+            if (tooltip.body) {
+                var titleLines = tooltip.title || [];
+                var bodyLines = tooltip.body.map(getBody);
+
+                var innerHtml = '<thead>';
+
+                titleLines.forEach(function (title) {
+                    innerHtml += '<tr><th>' + title + '</th></tr>';
+                });
+                innerHtml += '</thead><tbody>';
+
+                bodyLines.forEach(function (body, i) {
+                    if (body.length > 0) {
+                        var colors = tooltip.labelColors[i];
+                        var style = 'background:' + colors.backgroundColor;
+                        style += '; border-color:' + colors.borderColor;
+                        style += '; border-width: 2px';
+                        var span = '<span class="chartjs-tooltip-key" style="' + style + '"></span>';
+                        innerHtml += '<tr><td>' + span + body + '</td></tr>';
+                    }
+                });
+                innerHtml += '</tbody>';
+
+                var tableRoot = tooltipEl.querySelector('table');
+                tableRoot.innerHTML = innerHtml;
+            }
+
+            var positionY = this._chart.canvas.offsetTop;
+            var positionX = this._chart.canvas.offsetLeft;
+
+            var canvasWidth = element[0].style.width;
+            canvasWidth = canvasWidth.substr(0, canvasWidth.length - 2);
+
+            var ctxWidth = parseInt(canvasWidth);
+            var caretX = parseInt(tooltip.caretX);
+            var isNearEdge = (ctxWidth - caretX) < 40;
+
+            // default value for tooltip left
+            var tooltipLeft = positionX + tooltip.caretX + 10 + 'px';
+
+            // if near edge, reduce X padding
+            if (isNearEdge) {
+                tooltipLeft = positionX + tooltip.caretX - 30 + 'px';
+            }
+
+            // Display, position, and set styles for font
+            tooltipEl.style.opacity = 1;
+            tooltipEl.style.left = tooltipLeft;
+            tooltipEl.style.top = positionY + tooltip.caretY + 'px';
+            tooltipEl.style.fontFamily = tooltip._bodyFontFamily;
+            tooltipEl.style.fontSize = tooltip.bodyFontSize;
+            tooltipEl.style.fontStyle = tooltip._fontStyle;
+            tooltipEl.style.padding = tooltip.yPadding + 'px ' + tooltip.xPadding + 'px';
+        };
+
         function renderTimelineChart() {
             var canvas = element[0];
 
@@ -275,13 +360,17 @@
                 responsive: true,
                 maintainAspectRatio: false,
                 tooltips: {
+                    enabled: false,
                     mode: 'index',
                     position: 'nearest',
+                    bodyFontFamily: "'Calibri', 'Arial', sans-serif",
+                    custom: customTooltips,
                     callbacks: {
                         title: onTooltipsTitleCallback,
                         label: onTooltipsLabelCallback
                     }
                 },
+
                 scales: {
                     xAxes: [{
                         display: true,
