ca6ef7750b562ef13643ca214019a72721ff5b85
Mehdi K <khajoo@gmail.com>
2017-08-14 20:18:59 +0100

    Remove Right to Left and Farsi language support

--- LightSurveys.App/source/scripts/controllers/indexController.js
+++ LightSurveys.App/source/scripts/controllers/indexController.js
@@ -4,23 +4,23 @@ angular.module('lm.surveys').controller('indexController', ["$scope", "$rootScop
 
         $scope.authentication = authService.authentication;
         $scope.getDirection = function () {
-            return userService.current.language === 'fa-IR' ? 'rtl' : 'ltr';
+            return 'ltr';
         }
 
         $scope.getLeft = function () {
-            return $scope.getDirection() === 'rtl' ? 'right' : 'left';
+            return 'left';
         }
 
         $scope.getRight = function () {
-            return $scope.getDirection() === 'rtl' ? 'left' : 'right';
+            return 'right';
         }
 
         $scope.getBack = function () {
-            return $scope.getDirection() === 'rtl' ? 'forward' : 'back';
+            return 'back';
         }
 
         $scope.getForward = function () {
-            return $scope.getDirection() === 'rtl' ? 'back' : 'forward';
+            return 'forward';
         }
 
         if (!$scope.authentication.isAuth) {
