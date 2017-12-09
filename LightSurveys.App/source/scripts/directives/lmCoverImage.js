b20edff27cb3f653beaafdae8002018b0b193002
Armin Zia <arminzia@live.com>
2017-12-09 18:08:19 +0330

    added a new directive for setting background images. updated attachments view to use placeholders for media thumbnails.

---
+++ LightSurveys.App/source/scripts/directives/lmCoverImage.js
@@ -0,0 +1,14 @@
+﻿angular.module('lm.surveys').directive('coverImage', [function () {
+    return function (scope, elem, attrs) {
+        attrs.$observe('coverImage', function (value) {
+            elem.css({
+                'background-image': 'url(' + value + ')',
+                'background-size': 'cover',
+                'background-repeat': 'no-repeat',
+                'background-position': 'center',
+                'background-attachment': 'scroll',
+                'width': '100%'
+            });
+        });
+    }
+}]);
